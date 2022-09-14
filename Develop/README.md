9f61c1f3ef5c35558c425ccc8d2353c10f9a7015
ckrum21 <108544020+ckrum21@users.noreply.github.com>
2022-09-14 14:39:47 -0400

    questions and readme page

---
+++ Develop/README.md
@@ -0,0 +1,35 @@
+# <a>
+
+  ## Description
+  b
+  
+  ## Table of Contents
+  
+  ## Installation
+  c
+
+  ## Usage
+  d
+
+  ## Contribute Guidelines
+  e
+
+  ## Tests
+  f
+
+  ## Credits
+  
+  
+  ## License
+  
+  
+  ## Badges
+  
+  ## Features
+  
+  
+
+  
+  
+
+  
--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,85 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateREADME = ({title, description, installation, usageInformation, contributionGuidelines, testInstructions}) =>
+  `# <${title}>
+
+  ## Description
+  ${description}
+  
+  ## Table of Contents
+  
+  ## Installation
+  ${installation}
+
+  ## Usage
+  ${usageInformation}
+
+  ## Contribute Guidelines
+  ${contributionGuidelines}
+
+  ## Tests
+  ${testInstructions}
+
+  ## Credits
+  
+  
+  ## License
+  
+  
+  ## Badges
+  
+  ## Features
+  
+  
+
+  
+  
+
+  `;
 
 // TODO: Create an array of questions for user input
-const questions = [];
+inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'title',
+      message: 'What is title of your project?',
+    },
+    {
+      type: 'input',
+      name: 'description',
+      message: 'What is the description of your project?',
+    },
+    {
+      type: 'input',
+      name: 'installation',
+      message: 'What is the installation instructions for your project?',
+    },
+    {
+      type: 'input',
+      name: 'usageInformation',
+      message: 'What is the usage instructions for your project?',
+    },
+    {
+      type: 'input',
+      name: 'contributionGuidelines',
+      message: 'What is the contribution guidelines of your project?',
+    },
+    {
+      type: 'input',
+      name: 'testInstructions',
+      message: 'What is the test instructions for your website?',
+    },
+  ])
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
 // TODO: Create a function to initialize app
-function init() {}
+.then((answers) => {
+    const readmePageContent = generateREADME(answers);
 
-// Function call to initialize app
-init();
+    fs.writeFile('README.md', readmePageContent, (err) =>
+      err ? console.log(err) : console.log('Successfully created README.md!')
+    );
+  });
